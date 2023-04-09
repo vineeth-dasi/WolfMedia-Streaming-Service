0950f46de4cb946fc0f844065bfcc14b89334644
Mithila Reddy Tatigotla <mithilareddy1999@gmail.com>
2023-04-09 18:37:27 -0400

    Updated DBConnect

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -6,8 +6,8 @@ import java.sql.Statement;
 
 public class DBConnect {
 	public static Connection connection = null;
-	public static Statement statement = null;
-	public static ResultSet result = null;
+	// public static Statement statement = null;
+	// public static ResultSet result = null;
 	public static String url = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/";
 	public static String user = "vdasi";
 	public static String pswd = "200473135";
@@ -25,45 +25,45 @@ public class DBConnect {
 		}
 	}
 	
-	public static boolean execUpdate(String command) {
-		try {
-			statement.executeUpdate(command);
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Failure: " + e);
-			return false;
-		}
-	}
+	// public static boolean execUpdate(String command) {
+	// 	try {
+	// 		statement.executeUpdate(command);
+	// 		return true;
+	// 	} catch (SQLException e) {
+	// 		System.out.println("Failure: " + e);
+	// 		return false;
+	// 	}
+	// }
 	
-	public static ResultSet execQuery(String command) throws SQLException{
-		result = statement.executeQuery(command);
-		return result;
-	}
+	// public static ResultSet execQuery(String command) throws SQLException{
+	// 	result = statement.executeQuery(command);
+	// 	return result;
+	// }
 	
-	public static void beginTransaction() {
-		try {
-			connection.setAutoCommit(false);
-		} catch (SQLException e) {
-			System.out.println("Failure: " + e);
-		}
-	}
+	// public static void beginTransaction() {
+	// 	try {
+	// 		connection.setAutoCommit(false);
+	// 	} catch (SQLException e) {
+	// 		System.out.println("Failure: " + e);
+	// 	}
+	// }
 	
-	public static void rollBackTransaction() {
-		try {
-			connection.rollback();
-			connection.setAutoCommit(true);
-		} catch (SQLException e) {
-			System.out.println("Failure: " + e);
-		}
-	}
+	// public static void rollBackTransaction() {
+	// 	try {
+	// 		connection.rollback();
+	// 		connection.setAutoCommit(true);
+	// 	} catch (SQLException e) {
+	// 		System.out.println("Failure: " + e);
+	// 	}
+	// }
 	
-	public static void commitTransaction() {
-		try {
-			connection.commit();
-			connection.setAutoCommit(true);
-		} catch (SQLException e) {
-			System.out.println("Failure: " + e);
-		}
-	}
+	// public static void commitTransaction() {
+	// 	try {
+	// 		connection.commit();
+	// 		connection.setAutoCommit(true);
+	// 	} catch (SQLException e) {
+	// 		System.out.println("Failure: " + e);
+	// 	}
+	// }
 
 }
