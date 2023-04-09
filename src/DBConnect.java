9b71608f165b9c723c8bf7a40be43eb169d81fb5
Mithila Reddy Tatigotla <mithilareddy1999@gmail.com>
2023-04-09 18:04:49 -0400

    Added transcation functions

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,9 +1,13 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnect {
-	public static Connection connection;
+	public static Connection connection = null;
+	public static Statement statement = null;
+	public static ResultSet result = null;
 	public static String url = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/";
 	public static String user = "vdasi";
 	public static String pswd = "200473135";
@@ -12,6 +16,7 @@ public class DBConnect {
 	{
 			try {
 			connection = DriverManager.getConnection(url, user, pswd);
+			statement = connection.createStatement();
 			System.out.println("Successfully connected to DB");
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -27,6 +32,47 @@ public class DBConnect {
 			} catch (Throwable whatever) {
 				System.out.println("ERROR closing connection" + whatever.toString());
 			}
+		}
+	}
+	
+	public static boolean execUpdate(String command) {
+		try {
+			statement.executeUpdate(command);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("Failure: " + e);
+			return false;
+		}
+	}
+	
+	public static ResultSet execQuery(String command) throws SQLException{
+		result = statement.executeQuery(command);
+		return result;
+	}
+	
+	public static void beginTransaction() {
+		try {
+			connection.setAutoCommit(false);
+		} catch (SQLException e) {
+			System.out.println("Failure: " + e);
+		}
+	}
+	
+	public static void rollBackTransaction() {
+		try {
+			connection.rollback();
+			connection.setAutoCommit(true);
+		} catch (SQLException e) {
+			System.out.println("Failure: " + e);
+		}
+	}
+	
+	public static void commitTransaction() {
+		try {
+			connection.commit();
+			connection.setAutoCommit(true);
+		} catch (SQLException e) {
+			System.out.println("Failure: " + e);
 		}
 	}
 
